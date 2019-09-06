0859f4509e5aa3105bba8ca1d710059631480445
marquesr1 <rafael.fmarques@outlook.com>
2019-09-06 13:52:12 -0300

    Refactoring to use 'react-navigate'

--- App.js
+++ App.js
@@ -1,34 +1,3 @@
-import React, {Component} from 'react';
-import {Platform, Text, View} from 'react-native';
-import axios from 'axios';
+import PeoplePage from './src/screens/PeoplePage';
 
-import Header from './src/components/Header';
-import PeopleList from './src/components/PeopleList';
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      people: [],
-    };
-  }
-
-  componentDidMount() {
-    axios.get('https://randomuser.me/api/?nat=br&results=5').then(response => {
-      const {results} = response.data;
-      this.setState({
-        people: results,
-      });
-    });
-  }
-
-  render() {
-    return (
-      <View>
-        <Header label={'Contatos'} />
-        <PeopleList people={this.state.people} />
-      </View>
-    );
-  }
-}
+export default PeoplePage;
---
+++ src/screens/PeoplePage.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Platform, Text, View } from 'react-native';
+import axios from 'axios';
+
+import Header from '../components/Header';
+import PeopleList from '../components/PeopleList';
+
+export default class PeoplePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://randomuser.me/api/?nat=br&results=5').then(response => {
+      const { results } = response.data;
+      this.setState({
+        people: results,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header label={'Contatos'} />
+        <PeopleList people={this.state.people} />
+      </View>
+    );
+  }
+}
